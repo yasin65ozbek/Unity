e8ab881278bfab9fb6126043529c0491fb25415b
Yasin ÖZBEK <39374741+yasin65ozbek@users.noreply.github.com>
2021-05-21 21:04:16 +0300

    Add files via upload

---
+++ instant_Clon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instant_Clon : MonoBehaviour
+{
+    public GameObject klonlanacakNesne;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(klonlanacakNesne, transform.position, Quaternion.identity); //kopyalanacakNesne, pozisyon, açı
+            //Quaternion.identity = açılarının sıfıt olmasını belirtir
+        }
+    }
+}
